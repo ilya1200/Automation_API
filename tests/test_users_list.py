f16ec09534df244efdeb60ca93d5f4aa21bdd8a4
ilya <ilya1200@gmail.com>
2020-12-15 13:41:44 +0200

    init

---
+++ tests/test_users_list.py
@@ -0,0 +1,11 @@
+import requests
+from assertpy import assert_that
+
+
+def test_users_list():
+    url = "https://reqres.in/api/users?page=2"
+    response = requests.get(url)
+    content = response.json()
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(content['total']).is_equal_to(12)
+
