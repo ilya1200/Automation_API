8178973d646e148eabaa85efe594782c90ad40e5
ilya <ilya1200@gmail.com>
2020-12-15 14:16:09 +0200

    fix

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,7 +2,7 @@ import requests
 from assertpy import assert_that
 
 
-def test_users_list():
+def test_users():
     url = "https://reqres.in/api/users?page=2"
     response = requests.get(url)
     content = response.json()
